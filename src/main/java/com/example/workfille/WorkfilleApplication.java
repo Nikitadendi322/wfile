e85146e8005f7bc7ba79e7f16ea8a553721cb30f
Никита <Ployka322@yandex.ru>
2023-07-01 10:22:41 +0700

    start

---
+++ src/main/java/com/example/workfille/WorkfilleApplication.java
@@ -0,0 +1,13 @@
+package com.example.workfille;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkfilleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkfilleApplication.class, args);
+    }
+
+}
